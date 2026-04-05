defb261855b84bf3554f2c7700457bc4ca40f7d3
Your Name <you@example.com>
2026-04-05 12:55:59 -0600

    successful build 2026-04-05 12:55

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -9,12 +9,14 @@
       image-nvim
       molten-nvim
       jupytext-nvim
+      auto-save-nvim
+      which-key-nvim
     ];
 
     extraPackages = with pkgs; [
-       imagemagick
-       python3Packages.jupytext  # <-- add this
-     ];
+      imagemagick
+      python3Packages.jupytext
+    ];
 
     extraLuaPackages = ps: with ps; [
       magick
@@ -33,10 +35,20 @@
       vim.opt.exrc = true
       vim.opt.secure = true
 
+      vim.opt.tabstop = 2
+      vim.opt.shiftwidth = 2
+      vim.opt.expandtab = true
+
+      vim.g.mapleader = " "
+      vim.g.maplocalleader = " "
+
       vim.cmd.colorscheme("retrobox")
 
       -- Ctrl+Backspace deletes previous word in insert mode
       vim.keymap.set("i", "<C-BS>", "<C-w>", { silent = true })
+
+      -- which-key
+      require("which-key").setup({})
 
       -- image.nvim (required before molten)
       require("image").setup({
@@ -58,22 +70,46 @@
       vim.g.molten_wrap_output = true
       vim.g.molten_output_win_max_height = 20
 
-      -- jupytext: auto-convert .ipynb to markdown for editing
+      -- auto-import outputs when kernel is ready
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MoltenInitPost",
+        callback = function()
+          vim.cmd("MoltenImportOutput")
+        end,
+      })
+
+      -- auto-export outputs on save for .ipynb files
+      vim.api.nvim_create_autocmd("BufWritePost", {
+        pattern = "*.ipynb",
+        callback = function()
+          vim.cmd("MoltenExportOutput!")
+        end,
+      })
+
+      -- jupytext
       require("jupytext").setup({
         style = "markdown",
         output_extension = "md",
         force_ft = "markdown",
       })
 
+      -- auto-save
+      require("auto-save").setup({
+        enabled = true,
+        execution_message = { message = "" },
+        events = { "InsertLeave", "TextChanged" },
+        debounce_delay = 1000,
+      })
+
       -- molten keymaps
-      vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>",        { desc = "Molten init kernel", silent = true })
-      vim.keymap.set("n", "<localleader>e",  ":MoltenEvaluateOperator<CR>", { desc = "Molten evaluate operator", silent = true })
-      vim.keymap.set("n", "<localleader>rl", ":MoltenEvaluateLine<CR>", { desc = "Molten evaluate line", silent = true })
-      vim.keymap.set("n", "<localleader>rr", ":MoltenReevaluateCell<CR>", { desc = "Molten re-evaluate cell", silent = true })
-      vim.keymap.set("v", "<localleader>r",  ":<C-u>MoltenEvaluateVisual<CR>gv", { desc = "Molten evaluate visual", silent = true })
-      vim.keymap.set("n", "<localleader>os", ":noautocmd MoltenEnterOutput<CR>", { desc = "Molten enter output", silent = true })
-      vim.keymap.set("n", "<localleader>oh", ":MoltenHideOutput<CR>",   { desc = "Molten hide output", silent = true })
-      vim.keymap.set("n", "<localleader>md", ":MoltenDelete<CR>",       { desc = "Molten delete cell", silent = true })
+      vim.keymap.set("n", "<leader>mi", ":MoltenInit<CR>",             { desc = "Molten init kernel", silent = true })
+      vim.keymap.set("n", "<leader>e",  ":MoltenEvaluateOperator<CR>", { desc = "Molten evaluate operator", silent = true })
+      vim.keymap.set("n", "<leader>rl", ":MoltenEvaluateLine<CR>",     { desc = "Molten evaluate line", silent = true })
+      vim.keymap.set("n", "<leader>rr", ":MoltenReevaluateCell<CR>",   { desc = "Molten re-evaluate cell", silent = true })
+      vim.keymap.set("v", "<leader>r",  ":<C-u>MoltenEvaluateVisual<CR>gv", { desc = "Molten evaluate visual", silent = true })
+      vim.keymap.set("n", "<leader>os", ":noautocmd MoltenEnterOutput<CR>", { desc = "Molten enter output", silent = true })
+      vim.keymap.set("n", "<leader>oh", ":MoltenHideOutput<CR>",       { desc = "Molten hide output", silent = true })
+      vim.keymap.set("n", "<leader>md", ":MoltenDelete<CR>",           { desc = "Molten delete cell", silent = true })
     '';
   };
 }
