02eaeb4cc1907b799dd08c6d10196a57af533f83
Your Name <you@example.com>
2026-04-27 18:20:50 -0600

    successful build 2026-04-27 18:20

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -8,8 +8,76 @@
       lualine.enable = true;
       auto-save.enable = true;
       gitgutter.enable = true;
-      comfy-line-numbers.enable = true;
 
+    };
+    lsp = {
+      servers = {
+        "*" = {
+          config = {
+            capabilities = {
+              textDocument = {
+                semanticTokens = {
+                  multilineTokenSupport = true;
+                };
+              };
+            };
+            root_markers = [
+              ".git"
+            ];
+          };
+        };
+	gopls = {
+          enable = true;
+	  packageFallback = true;
+	  config = {
+	    cmd = [ 
+	      "gopls"
+	    ];
+	    filetypes = [
+	      "go"
+	      "gomod"
+	    ];
+	    root_markers = [
+	      "go.mod"
+	    ];
+	  };
+	};
+        pylsp = {
+          enable = true;
+          packageFallback = true;
+          config = {
+            cmd = [ "pylsp" ];
+            filetypes = [ "python" ];
+            root_markers = [
+              "pyproject.toml"
+              "setup.py"
+              "setup.cfg"
+              "requirements.txt"
+              "pyrightconfig.json"
+	      ".git"
+            ];
+          };
+        };
+        
+        nixd = {
+          enable = true;
+          packageFallback = true;
+          config = {
+            cmd = [ "nixd" ];
+            filetypes = [ "nix" ];
+            root_markers = [
+              "flake.nix"
+              "flake.lock"
+              "default.nix"
+              "shell.nix"
+	      ".git"
+            ];
+          };
+        };
+        lua_ls = {
+          enable = true;
+        };
+      };
     };
   };
 }
