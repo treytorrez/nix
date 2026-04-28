6f1951cb0ce8411ed14ff5ef15f2238150c3bbf6
Your Name <you@example.com>
2026-04-27 20:22:28 -0600

    successful build 2026-04-27 20:22

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -3,30 +3,60 @@
   programs.nixvim = {
     enable = true;
 
+    # Performance optimizations
+    performance.byteCompileLua = {
+      enable = true;
+      configs = true;
+      initLua = true;
+      nvimRuntime = true;
+      plugins = true;
+    };
+
     colorschemes.gruvbox.enable = true;
+    
+    globals.mapleader = " ";
+
     plugins = {
+      # Lazy loading infrastructure
+      lz-n.enable = true;
+
+      # UI/UX
       lualine.enable = true;
       auto-save.enable = true;
-      which-key.enable = true;
+      which-key = {
+        enable = true;
+        settings = {
+          spec = [
+            { __unkeyed = "<leader>f"; group = "Find/File"; }
+            { __unkeyed = "<leader>g"; group = "Git"; }
+            { __unkeyed = "<leader>l"; group = "LSP"; }
+            { __unkeyed = "<leader>n"; group = "Notebook"; }
+            { __unkeyed = "<leader>q"; group = "Quarto"; }
+            { __unkeyed = "<leader>t"; group = "Toggle"; }
+            { __unkeyed = "<leader>b"; group = "Buffer"; }
+          ];
+        };
+      };
       neogit.enable = true;
       telescope.enable = true;
       fzf-lua.enable = true;
       markview.enable = true;
       mini-icons = {
-	enable = true;
+        enable = true;
         mockDevIcons = true;
       };
+
+      # Completion
       cmp = {
         enable = true;
         autoEnableSources = true;
-        
         settings = {
           sources = [
             { name = "nvim_lsp"; }
             { name = "path"; }
             { name = "buffer"; }
+            { name = "otter"; }  # For code blocks in notebooks
           ];
-          
           mapping = {
             "<C-Space>" = "cmp.mapping.complete()";
             "<C-e>" = "cmp.mapping.close()";
@@ -37,7 +67,72 @@
         };
       };
 
+      # Syntax highlighting
+      treesitter = {
+        enable = true;
+        settings = {
+          highlight.enable = true;
+          indent.enable = true;
+        };
+        lazyLoad.settings = {
+          event = ["BufReadPost" "BufNewFile"];
+        };
+      };
+
+      # Notebook/Quarto support
+      quarto = {
+        enable = true;
+        lazyLoad.settings = {
+          ft = "quarto";
+        };
+      };
+
+      # LSP in code blocks
+      otter = {
+        enable = true;
+        lazyLoad.settings = {
+          ft = ["quarto" "markdown"];
+        };
+      };
+
+      # Image rendering (Kitty support)
+      image = {
+        enable = true;
+        lazyLoad.settings = {
+          ft = ["quarto" "markdown" "vimwiki"];
+        };
+      };
+
+      # Jupyter kernel
+      molten = {
+        enable = true;
+        settings = {
+          image_provider = "image.nvim";
+          auto_open_output = true;
+          enter_output_behavior = "open_and_enter";
+          output_win_max_height = 20;
+          wrap_output = true;
+          virt_text_output = true;
+        };
+#---
+# Molten doesn't lazy load apparently :(
+#---
+#        lazyLoad.settings = {
+#          cmd = [
+#            "MoltenInit"
+#            "MoltenEvaluateOperator"
+#            "MoltenEvaluateLine"
+#            "MoltenEvaluateVisual"
+#            "MoltenReevaluateCell"
+#            "MoltenDelete"
+#            "MoltenShowOutput"
+#            "MoltenHideOutput"
+#          ];
+#        };
+      };
     };
+
+    # LSP Configuration
     lsp = {
       servers = {
         "*" = {
@@ -49,63 +144,301 @@
                 };
               };
             };
-            root_markers = [
-              ".git"
-            ];
+            root_markers = [".git"];
           };
         };
-	gopls = {
+
+        # Go
+        gopls = {
           enable = true;
-	  packageFallback = true;
-	  config = {
-	    cmd = [ 
-	      "gopls"
-	    ];
-	    filetypes = [
-	      "go"
-	      "gomod"
-	    ];
-	    root_markers = [
-	      "go.mod"
-	    ];
-	  };
-	};
+          packageFallback = true;
+          config = {
+            cmd = ["gopls"];
+            filetypes = ["go" "gomod"];
+            root_markers = ["go.mod"];
+          };
+        };
+
+        # Python
         pylsp = {
           enable = true;
           packageFallback = true;
           config = {
-            cmd = [ "pylsp" ];
-            filetypes = [ "python" ];
+            cmd = ["pylsp"];
+            filetypes = ["python"];
             root_markers = [
               "pyproject.toml"
               "setup.py"
               "setup.cfg"
               "requirements.txt"
               "pyrightconfig.json"
-	      ".git"
             ];
           };
         };
-        
+
+        # Nix
         nixd = {
           enable = true;
           packageFallback = true;
           config = {
-            cmd = [ "nixd" ];
-            filetypes = [ "nix" ];
+            cmd = ["nixd"];
+            filetypes = ["nix"];
             root_markers = [
               "flake.nix"
               "flake.lock"
               "default.nix"
               "shell.nix"
-	      ".git"
             ];
           };
         };
+
+        # R
+        r_language_server = {
+          enable = true;
+          packageFallback = true;
+          config = {
+            cmd = ["R" "--slave" "-e" "languageserver::run()"];
+            filetypes = ["r" "rmd" "quarto"];
+            root_markers = [
+              ".git"
+              "DESCRIPTION"
+              "renv.lock"
+            ];
+          };
+        };
+
+        # Lua
         lua_ls = {
           enable = true;
         };
       };
     };
+
+    # Keybindings - Spacemacs style
+    keymaps = [
+      # ===== Notebook Operations (n) =====
+      {
+        mode = "n";
+        key = "<leader>ni";
+        action = "<cmd>MoltenInit<cr>";
+        options.desc = "Initialize kernel";
+      }
+      {
+        mode = "n";
+        key = "<leader>nr";
+        action = "<cmd>MoltenEvaluateLine<cr>";
+        options.desc = "Run current line";
+      }
+      {
+        mode = "v";
+        key = "<leader>nr";
+        action = ":<C-u>MoltenEvaluateVisual<cr>gv";
+        options.desc = "Run selection";
+      }
+      {
+        mode = "n";
+        key = "<leader>nR";
+        action = "<cmd>MoltenReevaluateCell<cr>";
+        options.desc = "Re-run cell";
+      }
+      {
+        mode = "n";
+        key = "<leader>no";
+        action = "<cmd>MoltenShowOutput<cr>";
+        options.desc = "Show output";
+      }
+      {
+        mode = "n";
+        key = "<leader>nh";
+        action = "<cmd>MoltenHideOutput<cr>";
+        options.desc = "Hide output";
+      }
+      {
+        mode = "n";
+        key = "<leader>nd";
+        action = "<cmd>MoltenDelete<cr>";
+        options.desc = "Delete cell";
+      }
+      {
+        mode = "n";
+        key = "<leader>nx";
+        action = "<cmd>MoltenInterrupt<cr>";
+        options.desc = "Interrupt execution";
+      }
+
+      # ===== Quarto Operations (q) =====
+      {
+        mode = "n";
+        key = "<leader>qp";
+        action = "<cmd>QuartoPreview<cr>";
+        options.desc = "Preview document";
+      }
+      {
+        mode = "n";
+        key = "<leader>qc";
+        action = "<cmd>QuartoClosePreview<cr>";
+        options.desc = "Close preview";
+      }
+      {
+        mode = "n";
+        key = "<leader>qa";
+        action = "<cmd>QuartoActivate<cr>";
+        options.desc = "Activate Quarto";
+      }
+
+      # ===== LSP Operations (l) =====
+      {
+        mode = "n";
+        key = "<leader>ld";
+        action = "<cmd>lua vim.lsp.buf.definition()<cr>";
+        options.desc = "Go to definition";
+      }
+      {
+        mode = "n";
+        key = "<leader>lD";
+        action = "<cmd>lua vim.lsp.buf.declaration()<cr>";
+        options.desc = "Go to declaration";
+      }
+      {
+        mode = "n";
+        key = "<leader>lr";
+        action = "<cmd>lua vim.lsp.buf.references()<cr>";
+        options.desc = "Find references";
+      }
+      {
+        mode = "n";
+        key = "<leader>li";
+        action = "<cmd>lua vim.lsp.buf.implementation()<cr>";
+        options.desc = "Go to implementation";
+      }
+      {
+        mode = "n";
+        key = "<leader>lh";
+        action = "<cmd>lua vim.lsp.buf.hover()<cr>";
+        options.desc = "Hover documentation";
+      }
+      {
+        mode = "n";
+        key = "<leader>ls";
+        action = "<cmd>lua vim.lsp.buf.signature_help()<cr>";
+        options.desc = "Signature help";
+      }
+      {
+        mode = "n";
+        key = "<leader>la";
+        action = "<cmd>lua vim.lsp.buf.code_action()<cr>";
+        options.desc = "Code actions";
+      }
+      {
+        mode = "n";
+        key = "<leader>lf";
+        action = "<cmd>lua vim.lsp.buf.format()<cr>";
+        options.desc = "Format buffer";
+      }
+      {
+        mode = "n";
+        key = "<leader>ln";
+        action = "<cmd>lua vim.lsp.buf.rename()<cr>";
+        options.desc = "Rename symbol";
+      }
+
+      # ===== File/Find Operations (f) =====
+      {
+        mode = "n";
+        key = "<leader>ff";
+        action = "<cmd>Telescope find_files<cr>";
+        options.desc = "Find files";
+      }
+      {
+        mode = "n";
+        key = "<leader>fg";
+        action = "<cmd>Telescope live_grep<cr>";
+        options.desc = "Grep files";
+      }
+      {
+        mode = "n";
+        key = "<leader>fb";
+        action = "<cmd>Telescope buffers<cr>";
+        options.desc = "Find buffers";
+      }
+      {
+        mode = "n";
+        key = "<leader>fh";
+        action = "<cmd>Telescope help_tags<cr>";
+        options.desc = "Help tags";
+      }
+      {
+        mode = "n";
+        key = "<leader>fr";
+        action = "<cmd>Telescope oldfiles<cr>";
+        options.desc = "Recent files";
+      }
+
+      # ===== Git Operations (g) =====
+      {
+        mode = "n";
+        key = "<leader>gg";
+        action = "<cmd>Neogit<cr>";
+        options.desc = "Neogit status";
+      }
+      {
+        mode = "n";
+        key = "<leader>gc";
+        action = "<cmd>Neogit commit<cr>";
+        options.desc = "Commit";
+      }
+      {
+        mode = "n";
+        key = "<leader>gp";
+        action = "<cmd>Neogit push<cr>";
+        options.desc = "Push";
+      }
+      {
+        mode = "n";
+        key = "<leader>gl";
+        action = "<cmd>Neogit log<cr>";
+        options.desc = "Log";
+      }
+
+      # ===== Buffer Operations (b) =====
+      {
+        mode = "n";
+        key = "<leader>bd";
+        action = "<cmd>bdelete<cr>";
+        options.desc = "Delete buffer";
+      }
+      {
+        mode = "n";
+        key = "<leader>bn";
+        action = "<cmd>bnext<cr>";
+        options.desc = "Next buffer";
+      }
+      {
+        mode = "n";
+        key = "<leader>bp";
+        action = "<cmd>bprevious<cr>";
+        options.desc = "Previous buffer";
+      }
+
+      # Quick access keybinds (no prefix)
+      {
+        mode = "n";
+        key = "K";
+        action = "<cmd>lua vim.lsp.buf.hover()<cr>";
+        options.desc = "LSP Hover";
+      }
+      {
+        mode = "n";
+        key = "gd";
+        action = "<cmd>lua vim.lsp.buf.definition()<cr>";
+        options.desc = "Go to definition";
+      }
+      {
+        mode = "n";
+        key = "gr";
+        action = "<cmd>lua vim.lsp.buf.references()<cr>";
+        options.desc = "Find references";
+      }
+    ];
   };
 }
