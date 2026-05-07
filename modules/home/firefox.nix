66287de4c38a7cfa7cb146940bf9b537cbcc38e3
Trey Torrez <treytorrez@proton.me>
2026-05-07 06:46:42 -0600

    successful build 2026-05-07 06:46

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -11,6 +11,7 @@
     profiles = {
       default = {
         bookmarks = {
+          force = true;
           "Nix" = {
             toolbar = true;
             bookmarks = {
@@ -61,42 +62,42 @@
         "CanvasBlocker@kkapsner.de" = {
           # NEEDS CONFIG INSIDE EXTENSION SETTINGS
           default_area = "menupanel";
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4691016/canvasblocker-1.12.xpi";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4691016/latest.xpi";
           installation_mode = "force_installed";
           private_browsing = true;
         };
         # Privacy Badger
         "jid1-MnnxcxisBPnSXQ@jetpack" = {
           default_area = "menupanel";
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4700632/privacy_badger17-2026.2.20.xpi";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4700632/latest.xpi";
           installation_mode = "force_installed";
           private_browsing = true;
         };
         # ClearURL - remove tracking info
         "{74145f27-f039-47ce-a470-a662b129930a}" = {
           default_area = "menupanel";
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4432106/clearurls-1.27.3.xpi";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4432106/latest.xpi";
           installation_mode = "force_installed";
           private_browsing = true;
         };
         # Multi-Account Containers
         "@testpilot-containers" = {
           default_area = "menupanel";
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4627302/multi_account_containers-8.3.6.xpi";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4627302/latest.xpi";
           installation_mode = "force_installed";
           private_browsing = true;
         };
         # Proton Pass
         "78272b6fa58f4a1abaac99321d503a20@proton.me" = {
           default_area = "menupanel";
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4686427/proton_pass-1.34.2.xpi";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4686427/latest.xpi";
           installation_mode = "force_installed";
           private_browsing = true;
         };
         # Tridactyl
         "tridactyl.vim@cmcaine.co.uk" = {
           default_area = "menupanel";
-          install_url = "https://addons.mozilla.org/firefox/downloads/file/4704384/tridactyl_vim-1.24.5.xpi";
+          install_url = "https://addons.mozilla.org/firefox/downloads/file/4704384/latest.xpi";
           installation_mode = "force_installed";
           private_browsing = true;
         };
