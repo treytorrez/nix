e68f723788851e3554a4cb2ee47f81c282a2950c
Trey Torrez <treytorrez@proton.me>
2026-05-19 12:06:42 -0600

    successful build 2026-05-19 12:06

--- modules/home/nixvim.nix
+++ modules/home/nixvim.nix
@@ -8,6 +8,7 @@
       tabstop = 2; # Tab width is 2 spaces
       shiftwidth = 2; # Indent by 2 spaces
       softtabstop = 2; # Tab key inserts 2 spaces
+      exrc = true;
     };
     # Performance optimizations
     performance.byteCompileLua = {
