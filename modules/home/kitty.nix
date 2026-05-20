d422f5621b10cb17e971f3b216f290b62b4d6388
Trey Torrez <treytorrez@proton.me>
2026-05-19 21:30:49 -0600

    successful build 2026-05-19 21:30

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,13 +1,18 @@
 { pkgs, ... }:
 {
   programs.kitty = {
-  enable = true;
-  shellIntegration.enableZshIntegration = true;
+    enable = true;
+    shellIntegration.enableZshIntegration = true;
+    enableGitIntegration = true;
+    extraConfig = ''font_family      family='AtkynsonMono Nerd Font' style=Light
+bold_font        auto
+italic_font      family='AtkynsonMono Nerd Font' style='Light Italic'
+bold_italic_font auto'';
     font = {
-        package = pkgs.nerd-fonts.atkynson-mono;
-        name = "AtkynsonMono Nerd Font";
-	size = 25;
-      };
+      package = pkgs.nerd-fonts.atkynson-mono;
+      name = "AtkynsonMono Nerd Font";
+      size = 25;
+    };
     themeFile = "gruvbox-dark-hard";
   };
 }
