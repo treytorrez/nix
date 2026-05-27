271f0a25e757c5afcd6a91554c5bb15929e356e9
Trey Torrez <treytorrez@proton.me>
2026-05-26 20:00:01 -0600

    successful build 2026-05-26 20:00

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }:
+{ pkgs, lib,  ... }:
 {
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/schemes/base24/gruvbox-dark.yaml";
+  stylix.image = lib.fetchurl {
+    url = "https://w.wallhaven.cc/full/p9/wallhaven-p9pd23.png";
+    hash = "sha256-S/6kgloXiIYI0NblT6YVXfqELApbdHGsuYe6S4JoQwQ=";
+  };
   stylix.fonts = {
     serif = {
       package = pkgs.nerd-fonts.atkynson-mono;
