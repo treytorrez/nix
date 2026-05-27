3587c5f11fb7066527df598d1c427af1d4a32f7a
Trey Torrez <treytorrez@proton.me>
2026-05-26 20:46:47 -0600

    successful build 2026-05-26 20:46

--- modules/home/stylix.nix
+++ modules/home/stylix.nix
@@ -4,6 +4,17 @@
 
   # for some reason the download for themes for gnome keep failing so i simply turned it off
   stylix.targets.gnome.enable = false;
+  #config.stylix.targets.librewolf.profileNames` is not set. Declare profile
+  #names with 'config.stylix.targets.librewolf.profileNames = [
+  #"<PROFILE_NAME>" ];'.
+
+  stylix.targets.librewolf.profileNames = [
+    "default"
+    "school"
+    "work"
+    "pwas"
+    ];
+
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   stylix.image = builtins.fetchurl {
     url = "https://w.wallhaven.cc/full/p9/wallhaven-p9pd23.png";
