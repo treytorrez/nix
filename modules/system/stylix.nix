d8ed951c90a8bd661700e8aa8d732b3d03bfd1b0
Trey Torrez <treytorrez@proton.me>
2026-05-26 19:49:06 -0600

    successful build 2026-05-26 19:49

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark.yaml";
   stylix.fonts = {
     serif = {
       package = pkgs.nerd-fonts.atkynson-mono;
