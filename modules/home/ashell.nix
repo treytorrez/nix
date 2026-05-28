fbca1c49a3324419aa36f6aaa39d2b711acde92d
Trey Torrez <treytorrez@proton.me>
2026-05-28 07:54:10 -0600

    successful build 2026-05-28 07:54

--- modules/home/ashell.nix
+++ modules/home/ashell.nix
@@ -12,7 +12,10 @@ in
       modules = {
         left = [ "Workspaces" ];
         center = [ "WindowTitle" ];
-        right = [ "SystemInfo" "Settings" ];
+        right = [
+          "SystemInfo"
+          "Settings"
+        ];
       };
 
       window_title.mode = "Title";
@@ -27,20 +30,23 @@ in
       };
 
       settings = {
-        indicators = [ "Network" "Battery" ];
+        indicators = [
+          "Network"
+          "Battery"
+        ];
         battery_format = "IconAndPercentage";
         network_indicator_format = "Icon";
       };
 
-        #      appearance = {
-        #        style = "Solid";
+      appearance = {
+        style = "Solid";
         #        primary_color    = "#${c.base0D}";
         #        success_color    = "#${c.base0B}";
         #        text_color       = "#${c.base05}";
         #        danger_color     = { base = "#${c.base08}"; weak = "#${c.base09}"; };
         #        background_color = { base = "#${c.base00}"; weak = "#${c.base01}"; strong = "#${c.base03}"; };
         #        secondary_color  = { base = "#${c.base02}"; };
-      #      };
+      };
     };
   };
 
