ee3e2065b030dc069bfd190f2451d0dabcaf5886
Your Name <you@example.com>
2026-06-01 12:50:31 -0600

    successful build 2026-06-01 12:50

--- modules/home/ollama.nix
+++ modules/home/ollama.nix
@@ -2,11 +2,11 @@
 {
   services.ollama = {
     enable = true;
-      host = "0.0.0.0:11434";   # was probably "127.0.0.1:11434" by default
+      host = "0.0.0.0";   # was probably "127.0.0.1:11434" by default
       acceleration = null; # auto detecs rocm
     environmentVariables = {
       OLLAMA_FLASH_ATTENTION = "1";
-      HSA_OVERRIDE_GFX_VERSION = "10.3.2";
+      # HSA_OVERRIDE_GFX_VERSION = "10.3.2"; # WAS USED FOR COLAB STUFF
     };
   };
 }
