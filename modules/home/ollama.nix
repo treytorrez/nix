d88579735802a35fa45a6eafd640acb044a32e68
Your Name <you@example.com>
2026-06-01 11:53:00 -0600

    successful build 2026-06-01 11:53

--- modules/home/ollama.nix
+++ modules/home/ollama.nix
@@ -5,6 +5,7 @@
     environmentVariables = {
       OLLAMA_FLASH_ATTENTION = "1";
       HSA_OVERRIDE_GFX_VERSION = "10.3.2";
+      listenAddress = "0.0.0.0:11434";   # was probably "127.0.0.1:11434" by default
     };
   };
 }
