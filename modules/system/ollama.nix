77cec13a6804ebbdb558ec223abfca496ea82697
Your Name <you@example.com>
2026-06-02 19:01:58 -0600

    successful build 2026-06-02 19:01

--- modules/system/ollama.nix
+++ modules/system/ollama.nix
@@ -2,8 +2,8 @@
 {
   services.ollama = {
     enable = true;
-    package = pkgs.ollama-rocm;
-    rocmOverrideGfx = "10.3.2";
+    #package = pkgs.ollama-rocm;
+    #    rocmOverrideGfx = "10.3.2";
     host = "0.0.0.0";
     port = 11434;
   };
