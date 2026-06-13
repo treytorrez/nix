502a0a4596ba37f2e7979be4169e76b01c99a527
Trey Torrez <treytorrez@proton.me>
2026-06-12 18:31:44 -0600

    Successful build on laptop - 2026-06-12 18:31:44

---
+++ modules/system/stylix.nix
@@ -0,0 +1,40 @@
+{
+  pkgs,
+  config,
+  ...
+}:
+{
+  stylix.enable = true;
+
+  # for some reason the download for themes for gnome keep failing so i simply turned it off
+  stylix.targets.gnome.enable = false;
+  #config.stylix.targets.librewolf.profileNames` is not set. Declare profile
+  #names with 'config.stylix.targets.librewolf.profileNames = [
+  #"<PROFILE_NAME>" ];'.
+
+  stylix.cursor = {
+    package = pkgs.openzone-cursors;
+    name = "OpenZone_White";
+  };
+
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  #  stylix.image = builtins.fetchurl {
+  #    url = "https://w.wallhaven.cc/full/p9/wallhaven-p9pd23.png";
+  #    name = "wallhaven-p9pd23.png";
+  #    sha256 = "sha256:036gqhbf6s5ddgvfbgn6iqbzgizssyf7820m5815b2gd748jw8zc";
+  #  };
+  stylix.fonts = {
+    monospace = {
+      package = pkgs.nerd-fonts.atkynson-mono;
+      name = "AtkynsonMono NFM";
+    };
+    serif = config.stylix.fonts.monospace;
+    sansSerif = config.stylix.fonts.monospace;
+
+    emoji = {
+      package = pkgs.noto-fonts-monochrome-emoji;
+      name = "Noto Monochrome Emoji";
+    };
+  };
+
+}
