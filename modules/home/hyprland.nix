f2c2874e93c8c1c213d86018a2ed5152767ce07f
Trey Torrez <treytorrez@proton.me>
2026-06-14 23:41:28 -0600

    Successful build on laptop - 2026-06-14 23:41:28

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -126,8 +126,8 @@ in
 
         ",XF86MonBrightnessUp, exec, brightnessctl set 15%+"
         ",XF86MonBrightnessDown, exec, brightnessctl set 15%-"
-        ",XF86AudioRaiseVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ +15%"
-        ",XF86AudioLowerVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ -15%"
+        ",XF86AudioRaiseVolumectl set-sink-volume @DEFAULT_SINK@ +5%"
+        ",XF86AudioLowerVolume, exec, pactl set-sink-volume @DEFAULT_SINK@ -5%"
         ",XF86AudioMute, exec, pactl set-sink-mute @DEFAULT_SINK@ toggle"
       ];
 
