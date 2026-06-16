65b8ab08de0c770d6b292903766dc42250fc9dd6
Trey Torrez <treytorrez@proton.me>
2026-06-15 19:21:37 -0600

    Successful build on laptop - 2026-06-15 19:21:37

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -15,7 +15,7 @@
   stylix.cursor = {
     package = pkgs.openzone-cursors;
     name = "OpenZone_White";
-    size = 16;
+    size = 32;
   };
 
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
