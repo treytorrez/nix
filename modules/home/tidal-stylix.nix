a0f04910012036f5b5db588727814fd0a8c61958
Trey Torrez <treytorrez@proton.me>
2026-06-21 15:23:07 -0600

    Successful build on laptop - 2026-06-21 15:23:07

--- modules/home/tidal-stylix.nix
+++ modules/home/tidal-stylix.nix
@@ -3,28 +3,7 @@ let
   c = config.lib.stylix.colors.withHashtag;
 
   themeScss = pkgs.writeText "stylix-tidal.scss" ''
-@use "base" with (
-$bg-main:        ${c.base00},
-$bg-secondary:   ${c.base01},
-$bg-surface:     ${c.base02},
-$bg-elevated:    ${c.base03},
-$bg-hover:       ${c.base03},
-$bg-selection:   ${c.base02},
-$text-primary:   ${c.base05},
-$text-secondary: ${c.base06},
-$text-muted:     ${c.base04},
-$text-faint:     ${c.base03},
-$accent-primary: ${c.base0E},
-$accent-green:   ${c.base0B},
-$accent-red:     ${c.base08},
-$accent-blue:    ${c.base0D},
-$accent-purple:  ${c.base0E},
-$accent-cyan:    ${c.base0C},
-$accent-yellow:  ${c.base0A},
-$border-subtle:  ${c.base02},
-$border-dim:     ${c.base03}
-);
-@use "layout";
+
 
 @use "base-new" with (
 $bg-main:        ${c.base00},
@@ -47,22 +26,21 @@ $accent-yellow:  ${c.base0A},
 $border-subtle:  ${c.base02},
 $border-dim:     ${c.base03}
 );
-@use "layout-new";
   '';
 
   compiledCss = pkgs.runCommand "tidal-stylix.css"
     {
-      nativeBuildInputs = [ pkgs.sass ];
+      nativeBuildInputs = [ pkgs.dart-sass ];
     }
     ''
       cp -r ${pkgs.tidal-hifi.src}/src/themes ./themes
       chmod -R +w ./themes
       cp ${themeScss} ./themes/stylix.scss
       echo $(ls -R)
-      cat env-vars
-      sass --sourcemap=none ./themes/stylix.scss $out
+      cat env-vars ./themes/_base-new.scss
+      sass --no-source-map ./themes/stylix.scss $out
     '';
 in
 {
-  home.file.".config/tidal-hifi/themes/stylix.scss".source = compiledCss;
+  home.file.".config/tidal-hifi/themes/stylix.css".source = compiledCss;
 }
