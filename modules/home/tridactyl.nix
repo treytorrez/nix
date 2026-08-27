0b7e640d0376bee5119490fab818683b7339cad2
Trey Torrez <treytorrez@proton.me>
2026-08-26 23:28:09 -0600

    Add tridactyl home-manager module

---
+++ modules/home/tridactyl.nix
@@ -0,0 +1,28 @@
+{ ... }:
+let
+  tridactylrc = ''
+    " General Settings
+    set update.lastchecktime 1787615607462
+    set update.lastnaggedversion 1.24.6
+    set configversion 2.0
+    set smoothscroll true
+
+    " Binds
+    bind K tabprev
+    bind J tabnext
+
+    " Autocmds
+    autocmd DocStart undefined mode ignore
+    autocmd DocStart zstream.mov/media mode ignore
+
+    " For syntax highlighting see https://github.com/tridactyl/vim-tridactyl
+    " vim: set filetype=tridactyl
+  '';
+in
+{
+  home.file.".tridactylrc" = {
+    target = ".tridactylrc";
+    text = tridactylrc;
+  };
+
+}
