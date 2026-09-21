999974a956e7f38fc05e378217da27ebcd32086c
Trey Torrez <treytorrez@proton.me>
2026-09-21 16:03:17 -0700

    Add ayu-dark scheme with local wallpaper and enable gnome theming

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -8,7 +8,7 @@
 
   # for some reason the download for themes for gnome keep failing so i simply turned it off
   # TODO: Try re-enabling this?
-  stylix.targets.gnome.enable = false;
+  #stylix.targets.gnome.enable = false;
   #config.stylix.targets.librewolf.profileNames` is not set. Declare profile
   #names with 'config.stylix.targets.librewolf.profileNames = [
   #"<PROFILE_NAME>" ];'.
@@ -19,12 +19,8 @@
     size = 32;
   };
 
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-  stylix.image = builtins.fetchurl {
-    url = "https://w.wallhaven.cc/full/p9/wallhaven-p9pd23.png";
-    name = "wallhaven-p9pd23.png";
-    sha256 = "036gqhbf6s5ddgvfbgn6iqbzgizssyf7820m5815b2gd748jw8zc";
-  };
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+  stylix.image = ../../media/future-cubes-dark-mode.jpg;
   stylix.fonts = {
     monospace = {
       package = pkgs.nerd-fonts.atkynson-mono;
