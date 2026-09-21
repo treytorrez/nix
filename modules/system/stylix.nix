655f7f7d49349379a61bb4b2df3a8b63c37f64bc
Trey Torrez <treytorrez@proton.me>
2026-09-21 16:15:38 -0700

    refactor stylix config to use attrset and add autoEnable

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -4,34 +4,37 @@
   ...
 }:
 {
-  stylix.enable = true;
+  stylix = {
+    enable = true;
+    autoEnable = true;
 
-  # for some reason the download for themes for gnome keep failing so i simply turned it off
-  # TODO: Try re-enabling this?
-  #stylix.targets.gnome.enable = false;
-  #config.stylix.targets.librewolf.profileNames` is not set. Declare profile
-  #names with 'config.stylix.targets.librewolf.profileNames = [
-  #"<PROFILE_NAME>" ];'.
+    # for some reason the download for themes for gnome keep failing so i simply turned it off
+    # TODO: Try re-enabling this?
+    #stylix.targets.gnome.enable = false;
+    #config.stylix.targets.librewolf.profileNames` is not set. Declare profile
+    #names with 'config.stylix.targets.librewolf.profileNames = [
+    #"<PROFILE_NAME>" ];'.
 
-  stylix.cursor = {
-    package = pkgs.openzone-cursors;
-    name = "OpenZone_White";
-    size = 32;
-  };
-
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
-  stylix.image = ../../media/future-cubes-dark-mode.jpg;
-  stylix.fonts = {
-    monospace = {
-      package = pkgs.nerd-fonts.atkynson-mono;
-      name = "AtkynsonMono NFM";
+    cursor = {
+      package = pkgs.openzone-cursors;
+      name = "OpenZone_White";
+      size = 32;
     };
-    serif = config.stylix.fonts.monospace;
-    sansSerif = config.stylix.fonts.monospace;
 
-    emoji = {
-      package = pkgs.noto-fonts-monochrome-emoji;
-      name = "Noto Monochrome Emoji";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-dark.yaml";
+    image = ../../media/future-cubes-dark-mode.jpg;
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.atkynson-mono;
+        name = "AtkynsonMono NFM";
+      };
+      serif = config.stylix.fonts.monospace;
+      sansSerif = config.stylix.fonts.monospace;
+
+      emoji = {
+        package = pkgs.noto-fonts-monochrome-emoji;
+        name = "Noto Monochrome Emoji";
+      };
     };
   };
 
